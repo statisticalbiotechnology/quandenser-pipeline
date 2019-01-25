ccf02aa6775c17e9d265ceedeec755370ac22c90
Timothy Bergstrom <timothy.bergstrom@gmail.com>
2019-01-25 14:42:02 +0100

    Changed path

--- containers/msconvert.sh
+++ containers/msconvert.sh
@@ -3,7 +3,7 @@
 NEXTFLOW_OUTPUTS="-with-trace Nextflow_output/trace.txt -with-report Nextflow_output/report.html -with-timeline Nextflow_output/timeline.html -with-dag Nextflow_output/flowchart.html"
 NEXTFLOW_PIPELINE="msconvert.nf"
 CONFIG_LOCATION="../config/nf.config"  # CHANGE TO REAL PATH AFTER WIP
-SINGULARITY_IMAGE="singulqand.simg" # CHANGE TO REAL PATH AFTER WIP
+SINGULARITY_IMAGE="../singulqand.simg" # CHANGE TO REAL PATH AFTER WIP
 SINGULARITY_ENABLE="-with-singularity"
 STDOUT_FILE="Nextflow_output/stdout.txt"
 
