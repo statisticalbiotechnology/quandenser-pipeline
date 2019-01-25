457f632655f1dee27c5601360a9161071f690ada
Timothy Bergstrom <timothy.bergstrom@gmail.com>
2019-01-25 14:44:13 +0100

    Changed so it depends on downloaded nextflow

--- run_all.sh
+++ run_all.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 sudo singularity build singulqand.simg Singularity
-# curl -s https://get.nextflow.io | bash
-# ./nextflow main.nf -with-singularity singulqand.simg
-nextflow main.nf -with-singularity singulqand.simg
+curl -s https://get.nextflow.io | bash
+./nextflow main.nf -with-singularity singulqand.simg
