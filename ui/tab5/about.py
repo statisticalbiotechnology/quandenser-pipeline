eb7c476c3215d70c24d3b27143a56ae83eb753e4
Timothy Bergstrom <timothy.bergstrom@gmail.com>
2019-01-28 16:17:34 +0100

    Placeholder about tab

---
+++ ui/tab5/about.py
@@ -0,0 +1,8 @@
+from PySide2.QtWebEngineWidgets import QWebEngineView
+
+class about(QWebEngineView):
+
+    def __init__(self):
+        super(about,self).__init__(parent = None)
+        html_file = open("tab5/about.html")
+        self.setHtml(html_file.read())
