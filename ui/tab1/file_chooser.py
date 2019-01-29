6834f8bbe029237564a264aeb6d617b12eb728a6
Timothy Bergstrom <timothy.bergstrom@gmail.com>
2019-01-29 14:19:06 +0100

    Fixed so the file chooser can handle both ms and fasta files

--- ui/tab1/file_chooser.py
+++ ui/tab1/file_chooser.py
@@ -1,29 +1,50 @@
 import sys
-from PySide2.QtWidgets import QFileDialog, QPushButton, QTextEdit
+from PySide2.QtWidgets import QFileDialog, QPushButton, QTextEdit, QLineEdit
 from PySide2.QtGui import QIcon
-from difflib import SequenceMatcher
 
 class file_chooser(QPushButton):
 
-    def __init__(self):
+    def __init__(self, type='ms'):
         super(file_chooser,self).__init__(parent = None)
-        self.setText('Choose MS files')
+        self.type = type
+        if self.type=='ms':
+            self.setText('Choose MS files')
+        elif self.type=='fasta':
+            self.setText('Choose fasta file')
         self.clicked.connect(self.open_window)
 
     def open_window(self):
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
-        files, _ = QFileDialog.getOpenFileNames(self,
-                                                "QFileDialog.getOpenFileNames()", "","All Files (*);;Python Files (*.py)",
-                                                options=options)
+        if self.type=='ms':
+            ms_file_types = ["*.mzML",
+                             "*.D | *.d",
+                             "*.YEP",
+                             "*.BAF",
+                             "*.FID",
+                             "*.WIFF",
+                             "*.raw | *.RAW"]
+            choose_string = " | ".join(ms_file_types)
+            files, _ = QFileDialog.getOpenFileNames(self,
+                                                    "Choose MS files",
+                                                    "",
+                                                    f"MS files ({choose_string});;All Files (*)",
+                                                    options=options)
+        elif self.type=='fasta':
+            files, _ = QFileDialog.getOpenFileName(self,
+                                                   "Choose fasta files",
+                                                   "",
+                                                   "Fasta files (.*fasta | *.Fasta | *.FASTA);;All Files (*)",
+                                                   options=options)
         if files:
             self.display_in_file_viewer(files)
 
     def display_in_file_viewer(self, files):
         parent = self.parentWidget()
-        batch_file_viewer = parent.findChildren(QTextEdit)[0]
-        for file in files:
-            batch_file_viewer.append(file + '\t' + 'A')
-
-    def similar(self, a, b):
-        return SequenceMatcher(None, a, b).ratio()
+        if self.type=='ms':
+            batch_file_viewer = parent.findChildren(QTextEdit)[0]
+            for file in files:
+                batch_file_viewer.auto_assign(file)
+        elif self.type=='fasta':
+            database_file_viewer = parent.findChildren(QLineEdit)[0]
+            database_file_viewer.setText(files)
