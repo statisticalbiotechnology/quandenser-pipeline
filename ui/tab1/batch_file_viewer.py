420430e3a9455aaf19e2e4afbf7ac411137e9f6d
Timothy Bergstrom <timothy.bergstrom@gmail.com>
2019-01-29 14:20:13 +0100

    Fasta file viewer

--- ui/tab1/batch_file_viewer.py
+++ ui/tab1/batch_file_viewer.py
@@ -2,6 +2,8 @@ import sys
 from PySide2.QtWidgets import QTextEdit
 from PySide2.QtGui import QColor
 import os
+from difflib import SequenceMatcher
+
 
 class batch_file_viewer(QTextEdit):
     def __init__(self):
@@ -38,3 +40,36 @@ class batch_file_viewer(QTextEdit):
                 self.setTextColor(QColor("green"))  # Check if path
         else:
             self.setTextColor(QColor("red"))
+
+    def auto_assign(self, txt):
+        clean_txt = self.clean_path(txt)
+        print(clean_txt)
+        all_txt = str(self.toPlainText())  # Copy, dont use
+        all_txt = all_txt.split('\n')
+        assigned_labels = [i[-1] for i in all_txt if len(i.split('\t'))==2]
+        if len(all_txt) == 0:
+            self.append(txt + '\t' + 'A')
+            return
+        for line in all_txt:
+            clean_line = self.clean_path(line)
+            similarity = SequenceMatcher(None, clean_line, clean_txt).ratio()
+            if similarity > 0.8 and len(line.split('\t')) == 2:  # Prevent error if assigned yourself
+                label = line.split('\t')[-1]
+                self.append(txt + '\t' + label)
+                return
+        # If we get here, the script has not found a similar match
+        char_index = 65
+        while True:
+            if chr(char_index) in assigned_labels:
+                char_index += 1
+            else:
+                label = chr(char_index)  # Make as chr
+                break
+        self.append(txt + '\t' + label)
+
+    def clean_path(self, txt):
+        txt = txt.split('\t')[0]  # Get path
+        txt = txt.split(os.sep)[-1]  # Get filename
+        txt = txt.split('.')[0]  # Ignore extension
+        txt = ''.join([i for i in txt if 65<=ord(i)<=122])  # Remove all non-letter chars + digits
+        return txt
