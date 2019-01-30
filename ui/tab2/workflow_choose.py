3a61649521752b5972f564438079608e7052f3af
Timothy Bergstrom <timothy.bergstrom@gmail.com>
2019-01-30 17:32:51 +0100

    Minor changes to boxes

--- ui/tab2/workflow_choose.py
+++ ui/tab2/workflow_choose.py
@@ -4,14 +4,8 @@ class workflow_choose(QComboBox):
 
     def __init__(self):
         super(workflow_choose,self).__init__(parent = None)
-        self.addItem("C")
-        self.addItem("C++")
-        self.addItems(["Java", "C#", "Python"])
+        self.addItems(["Full", "Triqler", "MSconvert"])
         self.currentIndexChanged.connect(self.selectionchange)
 
     def selectionchange(self,i):
-        print("Items in the list are :")
-
-        for count in range(self.count()):
-            print(self.itemText(count))
         print("Current index",i,"selection changed ",self.currentText())
