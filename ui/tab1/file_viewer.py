c1e7aa51d250a40cdff1ef178a87ab8ac4f7309a
Timothy Bergstrom <timothy.bergstrom@gmail.com>
2019-01-30 17:31:55 +0100

    Changed name and added so you can load multiple types of files

---
+++ ui/tab1/file_viewer.py
@@ -0,0 +1,34 @@
+import sys
+from PySide2.QtWidgets import QLineEdit
+from PySide2.QtGui import QColor
+import os
+
+class file_viewer(QLineEdit):
+    def __init__(self, type='file', id=0):
+        super(file_viewer,self).__init__(parent = None)
+        self.type = type
+        self.id = id
+        self.textChanged.connect(self.check_text)
+
+    def check_text(self):
+        self.blockSignals(True)
+        all_txt = self.text()  # Copy, dont use
+        self.clear()
+        self.pick_color(all_txt)
+        self.blockSignals(False)
+
+    def pick_color(self, txt):
+        if self.type == 'file':
+            if os.path.isfile(txt) and txt.split('.')[-1] in ["Fasta", "fasta", "FASTA"]:
+                self.setStyleSheet("color: green;") # Check if path
+                self.setText(txt)
+            else:
+                self.setStyleSheet("color: red;") # Check if path
+                self.setText(txt)
+        elif self.type == 'directory':
+            if os.path.isdir(txt):
+                self.setStyleSheet("color: green;") # Check if path
+                self.setText(txt)
+            else:
+                self.setStyleSheet("color: red;") # Check if path
+                self.setText(txt)
