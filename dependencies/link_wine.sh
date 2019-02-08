cbe541a45e6765278fae0ddf666477da3ab59977
Timothy Bergstrom <timothy.bergstrom@gmail.com>
2019-02-08 13:31:39 +0100

    Added force create link in script

--- dependencies/link_wine.sh
+++ dependencies/link_wine.sh
@@ -1,3 +1,3 @@
 for object in `ls $WINEPREFIX`;
-    do ln -s -T $WINEPREFIX/$object /var/local/shared_wine/wineprefix64/$object;
+    do ln -sf -T $WINEPREFIX/$object /var/local/shared_wine/wineprefix64/$object;
 done
