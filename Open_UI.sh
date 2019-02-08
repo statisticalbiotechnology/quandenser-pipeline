3c7425a794e4c8a5f97c39268a4272dbc8298472
Timothy Bergstrom <timothy.bergstrom@gmail.com>
2019-02-08 16:43:52 +0100

    Testing basic pipe communication

--- Open_UI.sh
+++ Open_UI.sh
@@ -2,10 +2,16 @@
 echo "Starting quandenser-pipeline GUI"
 
 function read_command() {
-      return 1
+  str_old="IMAGE_LOCATION=.*"
+  str_replace="IMAGE_LOCATION=$(pwd)/SingulQuand.SIF"
+  sed -ri "s|${str_old}|${str_replace}|g" "/var/tmp/quandenser_pipeline_$USER/PIPE"
+  echo "Hello"
 }
 
 while true; do
-  singularity run --app quandenser_ui --bind $(pwd):$(pwd) --nv singulqand.simg
+  #singularity run --app quandenser_ui --bind $(pwd):$(pwd) --nv SingulQuand.SIF
+  #wait
+  echo "DONE"
+  read_command
   break
 done
