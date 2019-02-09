5d56d843a0885e0534d0c9b36ea2994b60799f40
Timothy <30318121+TimothyBergstrom@users.noreply.github.com>
2019-02-09 12:34:36 +0100

    Added framework to work with the named pipe

--- Open_UI.sh
+++ Open_UI.sh
@@ -1,17 +1,38 @@
 #!/bin/bash
-echo "Starting quandenser-pipeline GUI"
+cd "$(dirname "$0")"  # Go to dir where the script lies, this allows for links to work
+
+function PIPE_read() {
+  # grep: -o, only get match, cut: -d'=' deliminter and get second column
+  value=$(grep -o "$1=.*" "/var/tmp/quandenser_pipeline_$USER/PIPE" | cut -d'=' -f2)
+  echo $value
+}
+
+function PIPE_write() {
+  parameter="$1=.*"
+  value="$1=$2"
+  # sed: -i overwrite in file, -E = regular expressions
+  sed -i -E "s|${parameter}|${value}|g" "/var/tmp/quandenser_pipeline_$USER/PIPE"
+}
 
 function read_command() {
-  str_old="IMAGE_LOCATION=.*"
-  str_replace="IMAGE_LOCATION=$(pwd)/SingulQuand.SIF"
-  sed -ri "s|${str_old}|${str_replace}|g" "/var/tmp/quandenser_pipeline_$USER/PIPE"
-  echo "Hello"
+  exit_code=$(PIPE_read "exit_code")
+  echo $exit_code
+  # Exit codes:
+  # 0 = run button pressed --> run the nextflow pipeline
+  # 1 = soft exit, closed window --> Do not run anything
+  # 2 = hard exit, python crashed --> Rerun 3 times and stop if not working
+  if [ $exit_code == 0 ]; then
+    echo "Run"
+  elif [ $exit_code == 1 ]; then
+    echo "Soft exit"
+  elif [ $exit_code == 2 ]; then
+    echo "Hard exit"
+  fi
 }
 
 while true; do
   #singularity run --app quandenser_ui --bind $(pwd):$(pwd) --nv SingulQuand.SIF
   #wait
-  echo "DONE"
   read_command
   break
 done
