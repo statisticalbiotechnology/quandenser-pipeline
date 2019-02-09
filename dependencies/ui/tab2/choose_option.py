b6539733bffd058f5dad43a12a156d82582a68fc
Timothy <30318121+TimothyBergstrom@users.noreply.github.com>
2019-02-09 14:27:01 +0100

    Added so you can reuse the combo box for other parameters

--- dependencies/ui/tab2/choose_option.py
+++ dependencies/ui/tab2/choose_option.py
@@ -4,14 +4,19 @@ from ..custom_config_parser import custom_config_parser
 
 class choose_option(QComboBox):
 
-    def __init__(self, settings_file):
+    def __init__(self, settings_file, parameter):
         super(choose_option,self).__init__(parent = None)
-        self.addItems(["Full", "Triqler", "MSconvert"])
+        self.parameter = parameter
+        if self.parameter == 'workflow':
+            self.addItems(["Full", "MSconvert"])
+        elif self.parameter == 'parallell_msconvert':
+            self.addItems(["true", "false"])
         self.parser = custom_config_parser()
         self.parser.load(settings_file)
         self.currentIndexChanged.connect(self.selectionchange)
 
     def selectionchange(self,i):
-        #print("Current index",i,"selection changed ",self.currentText())
-        self.parser.write("params.workflow_choose", self.currentText())
-        value = self.parser.get("params.workflow_choose")
+        if self.parameter == 'workflow':
+            self.parser.write("params.workflow", self.currentText())
+        elif self.parameter == 'parallell_msconvert':
+            self.parser.write("params.parallell_msconvert", self.currentText(), isString=False)
