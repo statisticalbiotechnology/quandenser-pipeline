8db8ae66c53d838eb150672f31de28558a6934e0
Timothy <30318121+TimothyBergstrom@users.noreply.github.com>
2019-02-09 12:47:27 +0100

    Moved ui and configs to dependencies folder

--- ui/tab5/about.py
+++
@@ -1,8 +0,0 @@
-from PySide2.QtWebEngineWidgets import QWebEngineView
-
-class about(QWebEngineView):
-
-    def __init__(self):
-        super(about,self).__init__(parent = None)
-        html_file = open("ui/tab5/about.html", encoding='utf-8')
-        self.setHtml(html_file.read())
