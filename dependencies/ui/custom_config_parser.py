49dfee3f040cfd5aea1fa2ade3762bae9c4ba710
Timothy <30318121+TimothyBergstrom@users.noreply.github.com>
2019-02-10 10:53:49 +0100

    Fixed bug where the contents were only read once, not during reading file

--- dependencies/ui/custom_config_parser.py
+++ dependencies/ui/custom_config_parser.py
@@ -7,42 +7,49 @@ class custom_config_parser():
 
     def load(self, file):
         self.settings_file = file
-        self.settings = []
+
+    def read_lines():
+        settings = []
         for line in open(self.settings_file, 'r'):
-            self.settings.append(line)
+            settings.append(line)
+        return settings
 
     def get(self, parameter):
-        index = [self.settings.index(i) for i in self.settings if i.startswith(parameter)][0]
+        settings = self.read_lines()
+        index = [settings.index(i) for i in settings if i.startswith(parameter)][0]
         # syntax:  <parameter>=<input>
-        value = self.settings[index].split('=')
+        value = settings[index].split('=')
         if len(value) != 2:  # Check for blank spaces in parameter file
-            value = self.settings[index].split(' = ')[-1]
+            value = settings[index].split(' = ')[-1]
         else:
             value = value[-1]
         value = value.replace('"', '')
         return value
 
     def write(self, parameter, value, isString=True):
+        settings = self.read_lines()
         value = str(value)
-        index = [self.settings.index(i) for i in self.settings if i.startswith(parameter)][0]
+        index = [settings.index(i) for i in settings if i.startswith(parameter)][0]
         if isString:  # Need "
             value = '"' + value + '"'
-        self.settings[index] = f'{parameter}={value}\n'  # Need to add \n here
-        with open(self.settings_file, 'w') as file:
-            for line in self.settings:
+        settings[index] = f'{parameter}={value}\n'  # Need to add \n here
+        with open(settings_file, 'w') as file:
+            for line in settings:
                 file.write(line)
         return True
 
     def get_params():
+        settings = self.read_lines()
         params = []
-        for line in self.settings:
+        for line in settings:
             if not line.startswith('#') and '=' in line:
                 params.append(line.split('=')[0])
         return params
 
     def get_values():
+        settings = self.read_lines()
         values = []
-        for line in self.settings:
+        for line in settings:
             if not line.startswith('#') and '=' in line:
                 values.append(line.split('=')[0])
         return values
