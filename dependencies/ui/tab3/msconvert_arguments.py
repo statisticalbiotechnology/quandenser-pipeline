e33385e634668515a9230b0d0a9c22b9be8a9370
Timothy Bergstrom <timothy.bergstrom@gmail.com>
2019-02-11 14:03:52 +0100

    Added so it will write to config file

--- dependencies/ui/tab3/msconvert_arguments.py
+++ dependencies/ui/tab3/msconvert_arguments.py
@@ -1,10 +1,20 @@
 import sys
 from PySide2.QtWidgets import QLineEdit
 
-class msconvert_arguments(QLineEdit):
-    def __init__(self):
-        super(msconvert_arguments,self).__init__(parent = None)
-        self.setText('')
+# Custom parser for both sh files and nf configs
+from ..custom_config_parser import custom_config_parser
 
-    def run(self):
-        return self.text()
+class msconvert_arguments(QLineEdit):
+    def __init__(self, nf_settings_path):
+        super(msconvert_arguments,self).__init__(parent = None)
+        self.nf_settings_path = nf_settings_path
+        self.nf_settings_parser = custom_config_parser()
+        self.nf_settings_parser.load(self.nf_settings_path)
+        self.setText('')
+        self.textChanged.connect(self.check_text)
+
+    def check_text(self):
+        self.blockSignals(True)
+        all_txt = self.text()  # Copy, dont use
+        self.nf_settings_parser.write("params.msconvert_additional_arguments", all_txt)
+        self.blockSignals(False)
