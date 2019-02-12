3dda3036518e96831fc6933632abbd7c692402c8
Timothy Bergstrom <timothy.bergstrom@gmail.com>
2019-02-12 13:36:08 +0100

    Added so the choose option can hide cluster options if not selected

--- dependencies/ui/tab2/choose_option.py
+++ dependencies/ui/tab2/choose_option.py
@@ -1,4 +1,4 @@
-from PySide2.QtWidgets import QComboBox, QVBoxLayout
+from PySide2.QtWidgets import QComboBox, QVBoxLayout, QFrame
 
 from ..custom_config_parser import custom_config_parser
 
@@ -11,6 +11,8 @@ class choose_option(QComboBox):
             self.addItems(["Full", "MSconvert"])
         elif self.parameter == 'parallell_msconvert':
             self.addItems(["true", "false"])
+        elif self.parameter == 'profile':
+            self.addItems(["local", "cluster"])
         self.parser = custom_config_parser()
         self.parser.load(settings_file)
         self.currentIndexChanged.connect(self.selectionchange)
@@ -20,3 +22,19 @@ class choose_option(QComboBox):
             self.parser.write("params.workflow", self.currentText())
         elif self.parameter == 'parallell_msconvert':
             self.parser.write("params.parallell_msconvert", self.currentText(), isString=False)
+        elif self.parameter == 'profile':
+            self.parser.write("PROFILE", self.currentText())
+            self.check_hidden()
+
+    def check_hidden(self):
+        parent = self.parentWidget()
+        parent = parent.parentWidget()  # Exist two levels up
+        children = parent.children()
+        if self.currentText() == "cluster":  # Add widgets
+            for child in children:
+                if hasattr(child, "hidden_object"):
+                    child.show()
+        elif self.currentText() == "local":
+            for child in children:
+                if hasattr(child, "hidden_object"):
+                    child.hide()
