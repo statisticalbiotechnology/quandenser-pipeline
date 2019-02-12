f5de2933f49c807a2b6434794b4c0054964adc85
Timothy Bergstrom <timothy.bergstrom@gmail.com>
2019-02-12 09:56:08 +0100

    Changed so running jobs are in a table instead

--- dependencies/ui/tab4/running_jobs.py
+++ dependencies/ui/tab4/running_jobs.py
@@ -1,5 +1,5 @@
 import os
-from PySide2.QtWidgets import QTextBrowser
+from PySide2.QtWidgets import QTextBrowser, QTableWidget, QHeaderView, QTableWidgetItem, QPushButton
 import subprocess
 from PySide2.QtGui import QColor
 
@@ -7,14 +7,40 @@ from PySide2.QtGui import QColor
 # Custom parser for both sh files and nf configs
 from ..custom_config_parser import custom_config_parser
 
-class running_jobs(QTextBrowser):
+class running_jobs(QTableWidget):
 
     def __init__(self, jobs_path):
         super(running_jobs,self).__init__(parent = None)
         self.jobs_path = jobs_path
 
+        jobs = []
+        for line in open(self.jobs_path, 'r'):
+            job = line.split('\t')  # First is pid
+            jobs.append(job)
+
+        self.setRowCount(len(jobs))
+        self.setColumnCount(4)
+        # Fill all places so there are no "None" types in the table
+        for row in range(self.rowCount()):
+            for column in range(self.columnCount()):
+                item = QTableWidgetItem()
+                item.setText('')
+                self.setItem(row, column, item)
+        self.header = self.horizontalHeader()
+        self.header.setSectionResizeMode(2, QHeaderView.Stretch)
+        self.setHorizontalHeaderLabels(["Pid", "Running", "Path", "Kill"])
+
+    def keyPressEvent(self, event):
+        """Add functionallity to keyboard"""
+        if event.key() == 16777221 or event.key() == 16777220:  # *.221 is right enter
+            if len(self.selectedIndexes()) == 0:  # Quick check if anything is selected
+                pass
+            else:
+                index = self.selectedIndexes()[0]  # Take last
+                self.setCurrentCell(index.row() + 1, index.column())
+        super().keyPressEvent(event)  # Propagate to built in methods
+
     def update(self):
-        self.clear()
         processes = []
         jobs = []
         for line in open(self.jobs_path, 'r'):
@@ -25,13 +51,33 @@ class running_jobs(QTextBrowser):
                                         shell=True)
             processes.append(process)
 
+        # If more jobs than rows
+        if len(jobs) >= self.rowCount():
+            self.setRowCount(len(jobs))
+            for row in range(self.rowCount()):
+                for column in range(self.columnCount()):
+                    item = QTableWidgetItem()
+                    item.setText('')
+                    self.setItem(row, column, item)    # Note: new rowcount here
+
+        row = 0
         for job, process in zip(jobs, processes):
             out, err = process.communicate()  # Wait for process to terminate
             out = out.decode("utf-8")
             out = out.split('\t')
-            if any("run_quandenser.sh" in line for line in out):
-                self.setTextColor(QColor("red"))
-                self.append(f"{job[0]} RUNNING {job[1]} {job[2]}")
-            else:
-                self.setTextColor(QColor("green"))
-                self.append(f"{job[0]} COMPLETED {job[1]} {job[2]}")
+            for column in range(self.columnCount()):
+                item = self.item(row, column)
+                if column == 1:
+                    if any("run_quandenser.sh" in line for line in out):
+                        item.setForeground(QColor('red'))
+                        item.setText("RUNNING")
+                    else:
+                        print("Test")
+                        item.setForeground(QColor('green'))
+                        item.setText("COMPLETED")
+                elif column == 3:
+                    if self.item(row, 1).text() == "RUNNING":
+                        self.setCellWidget(row, column, QPushButton('KILL'))
+                else:
+                    item.setText(job[column].replace('\n', ''))
+            row += 1
