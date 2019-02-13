1ad9bea69d207d5abf59b2ba91882f64aa290671
Timothy Bergstrom <timothy.bergstrom@gmail.com>
2019-02-13 10:57:47 +0100

    Changed (once again) image to html. This time, no js errors! :D

--- dependencies/ui/tab2/workflow.py
+++ dependencies/ui/tab2/workflow.py
@@ -1,11 +1,23 @@
-from PySide2.QtWidgets import QLabel
-from PySide2.QtGui import QPixmap
+from PySide2.QtWebEngineWidgets import QWebEngineView, QWebEnginePage
 
-
-class workflow(QLabel):
+class workflow(QWebEngineView):
 
     def __init__(self):
         super(workflow,self).__init__(parent = None)
-        pixmap = QPixmap('ui/tab2/flowchart.png')
-        self.setScaledContents(True)
-        self.setPixmap(pixmap)
+        self.page = page()
+        self.setPage(self.page)
+        self.page.load_page()  # Must be in this order
+        self.setZoomFactor(1)  # Title size
+
+class page(QWebEnginePage):
+
+    def __init__(self):
+        super(page,self).__init__(parent = None)
+
+    def load_page(self):
+        html_file = open("ui/tab2/flowchart.html", encoding='utf-8')
+        self.setHtml(html_file.read())
+
+    def javaScriptConsoleMessage(self, level, message, lineNumber, sourceId):
+        if not "a parser-blocking" in message and level == 2:  # Filter script injection erros
+            print(level, message, lineNumber, sourceId)
