1bce3ef1d37e112db52182b099d9abab37d3a628
Timothy Bergstrom <timothy.bergstrom@gmail.com>
2019-02-13 16:22:56 +0100

    Added missing "read" to html file

--- dependencies/ui/tab5/about.py
+++ dependencies/ui/tab5/about.py
@@ -1,8 +1,10 @@
 from PySide2.QtWebEngineWidgets import QWebEngineView
+from PySide2.QtCore import QUrl
+import os
 
 class about(QWebEngineView):
 
     def __init__(self):
         super(about,self).__init__(parent = None)
-        html_file = open("ui/tab5/about.html", encoding='utf-8')
+        html_file = open("ui/tab5/about.html", 'r', encoding='utf-8')
         self.setHtml(html_file.read())
