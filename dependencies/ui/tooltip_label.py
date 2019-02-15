15b669dc161fce082c3b42b83397f4f6836b81d0
Timothy Bergstrom <timothy.bergstrom@gmail.com>
2019-02-15 17:03:02 +0100

    Fixed so you can use the tooltip label

--- dependencies/ui/tooltip_label.py
+++ dependencies/ui/tooltip_label.py
@@ -1,7 +1,12 @@
 from PySide2.QtWidgets import QLabel
+from PySide2 import QtCore
 
 class tooltip_label(QLabel):
 
-    def __init__(self, tooltip):
+    def __init__(self, text, tooltip, style=True):
         super(tooltip_label,self).__init__(parent = None)
+        self.setText(text)
         self.setToolTip(tooltip)
+        if style:
+            self.setStyleSheet("font: 30pt rockwell")
+            self.setAlignment(QtCore.Qt.AlignCenter)
