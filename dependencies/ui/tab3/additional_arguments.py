fd34cb8b9a1afd352c564655eb9ace24868e87be
Timothy Bergstrom <timothy.bergstrom@gmail.com>
2019-02-18 14:32:05 +0100

    Changed name and added so multiple extra arguments can be used

--- dependencies/ui/tab3/additional_arguments.py
+++ dependencies/ui/tab3/additional_arguments.py
@@ -4,17 +4,18 @@ from PySide2.QtWidgets import QLineEdit
 # Custom parser for both sh files and nf configs
 from custom_config_parser import custom_config_parser
 
-class msconvert_arguments(QLineEdit):
-    def __init__(self, nf_settings_path):
-        super(msconvert_arguments,self).__init__(parent = None)
+class additional_arguments(QLineEdit):
+    def __init__(self, nf_settings_path, type='null'):
+        super(additional_arguments,self).__init__(parent = None)
         self.nf_settings_path = nf_settings_path
         self.nf_settings_parser = custom_config_parser()
         self.nf_settings_parser.load(self.nf_settings_path)
+        self.type = type
         self.setText('')
         self.textChanged.connect(self.check_text)
 
     def check_text(self):
         self.blockSignals(True)
         all_txt = self.text()  # Copy, dont use
-        self.nf_settings_parser.write("params.msconvert_additional_arguments", all_txt)
+        self.nf_settings_parser.write(f"params.{self.type}", all_txt)
         self.blockSignals(False)
