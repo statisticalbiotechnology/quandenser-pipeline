73357fd0762e78480946d74276bdb29c17546acb
Timothy Bergstrom <timothy.bergstrom@gmail.com>
2019-02-25 13:08:51 +0100

    Added so choose_option with parallel processes opens maxForks option

--- dependencies/ui/tab2/choose_option.py
+++ dependencies/ui/tab2/choose_option.py
@@ -1,10 +1,11 @@
-from PySide2.QtWidgets import QComboBox, QVBoxLayout, QFrame, QTableWidget
+from PySide2.QtWidgets import QComboBox, QVBoxLayout, QFrame, QTableWidget, QLabel
 
 from custom_config_parser import custom_config_parser
+from tab2.parameter_setter import parameter_setter_single
 
 class choose_option(QComboBox):
 
-    def __init__(self, settings_file, parameter):
+    def __init__(self, parameter, settings_file):
         super(choose_option,self).__init__(parent = None)
         self.parameter = parameter
         if self.parameter == 'workflow':
@@ -12,29 +13,47 @@ class choose_option(QComboBox):
         elif 'parallel' in self.parameter:
             self.addItems(["true", "false"])
         elif self.parameter == 'profile':
-            self.addItems(["local", "slurm_cluster"])
+            self.addItems(["local", "cluster"])
+        elif self.parameter == 'process.executor':
+            self.addItems(["slurm"])
         self.parser = custom_config_parser()
         self.parser.load(settings_file)
+        self.settings_file = settings_file
         self.default()
         self.currentIndexChanged.connect(self.selectionchange)
 
     def selectionchange(self,i):
         if self.parameter == 'workflow':
-            self.parser.write("params.workflow", self.currentText())
-            self.window()
-            window = self.window()
+            self.parser.write(f"params.{self.parameter}", self.currentText())
+            window = self.window()  # Update batch_file_viewers on tab 1
             self.recurse_children(window)
         elif 'parallel' in self.parameter:
             self.parser.write(f"params.{self.parameter}", self.currentText(), isString=False)
+            if not hasattr(self, 'max_forks_widget'):
+                self.max_forks_widget = parameter_setter_single(f"{self.parameter}_max_forks", self.settings_file)
+                self.label = QLabel(f"Max forks {self.parameter.replace('_', ' ')}")
+                parent = self.parent().layout()
+                parent.addRow(self.label, self.max_forks_widget)
+                self.max_forks_widget.hide()
+                self.label.hide()
+            if self.currentText() == "true":
+                self.max_forks_widget.show()
+                self.label.show()
+            else:
+                self.max_forks_widget.hide()
+                self.label.hide()
         elif self.parameter == 'profile':
             self.parser.write("PROFILE", self.currentText())
             self.check_hidden()
+        elif self.parameter == 'process.executor':
+            self.parser.write(f"{self.parameter}", self.currentText(),
+                              additional_information="cluster {")
 
     def check_hidden(self):
         parent = self.parentWidget()
         parent = parent.parentWidget()  # Exist two levels up
         children = parent.children()
-        if self.currentText() == "slurm_cluster":  # Add widgets
+        if self.currentText() == "cluster":  # Add widgets
             for child in children:
                 if hasattr(child, "hidden_object"):
                     child.show()
@@ -46,6 +65,8 @@ class choose_option(QComboBox):
     def default(self):
         if self.parameter == 'profile':
             index = self.findText(self.parser.get("PROFILE"))
+        elif self.parameter == 'process.executor':
+            index = self.findText(self.parser.get(f"{self.parameter}", additional_information="cluster {"))
         else:
             index = self.findText(self.parser.get(f"params.{self.parameter}"))
         self.setCurrentIndex(index)
