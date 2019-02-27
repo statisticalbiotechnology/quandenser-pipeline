d526c1a28e9d2454de66e486a8acf1d8829ef662
Timothy Bergstrom <timothy.bergstrom@gmail.com>
2019-02-27 15:45:45 +0100

    Added pipe to stderr as well

--- dependencies/ui/config/run_quandenser.sh
+++ dependencies/ui/config/run_quandenser.sh
@@ -15,12 +15,13 @@ SINGULARITY_ENABLE="-with-singularity"
 
 OUTPUT_PATH="$OUTPUT_PATH$OUTPUT_PATH_LABEL"  # Modify output path after workpath has been set
 STDOUT_FILE="$OUTPUT_PATH/stdout.txt"
+STDERR_FILE="$OUTPUT_PATH/stderr.txt"
 NEXTFLOW_OUTPUTS="-with-trace $OUTPUT_PATH/Nextflow_output/trace.txt -with-report $OUTPUT_PATH/Nextflow_output/report.html -with-timeline $OUTPUT_PATH/Nextflow_output/timeline.html -with-dag $OUTPUT_PATH/Nextflow_output/flowchart.html"
 
 mkdir -p "$OUTPUT_PATH/Nextflow_output"  # Will also create output folder if it does not exist
 
 $CONFIG_LOCATION/nextflow run $NEXTFLOW_PIPELINE $SINGULARITY_ENABLE $SINGULARITY_IMAGE \
--c $NF_CONFIG_LOCATION $NEXTFLOW_OUTPUTS -w $WORK_DIRECTORY -profile $PROFILE | tee $STDOUT_FILE
+-c $NF_CONFIG_LOCATION $NEXTFLOW_OUTPUTS -w $WORK_DIRECTORY -profile $PROFILE > $STDOUT_FILE 2>$STDERR_FILE
 
 # Notes: by using "-with-weblog", you could send the output to a url (HTTP POST request)
 # This means you could get notified with your webserver when the pipeline is finisheds
