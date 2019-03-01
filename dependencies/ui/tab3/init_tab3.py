42b98317cfa28d911b2e078ff6852b4a0affb24e
Timothy Bergstrom <timothy.bergstrom@gmail.com>
2019-03-01 13:00:39 +0100

    Prepare for resume directory

--- dependencies/ui/tab3/init_tab3.py
+++ dependencies/ui/tab3/init_tab3.py
@@ -4,10 +4,11 @@ from PySide2.QtWidgets import QWidget, QHBoxLayout, QVBoxLayout, QFormLayout, QL
 from tab3.additional_arguments import additional_arguments
 from tab3.parameter_setter import parameter_setter_double, parameter_setter_single
 from tab3.reset_button import reset_button
+from tab3.resume_chooser import resume_chooser
+from tab3.resume_folder_viewer import resume_folder_viewer
 
 from custom_config_parser import custom_config_parser
 from tooltip_label import tooltip_label
-
 
 def init_tab3(paths):
     tab3 = QWidget()
@@ -60,7 +61,13 @@ def init_tab3(paths):
     tooltip_label_dir_label = tooltip_label('Output directory label',
     """If you are running multiple runs in the same directory, this label will allow you to
     run many runs in the same output directory without overwriting the outputs""")
+
+    tab3_resume_button = resume_chooser(paths['pipe'])
+    tab3_resume_folder_viewer = resume_folder_viewer(paths['nf'])
+
     tab3_rightbox_layout.addRow(tooltip_label_dir_label, tab3_output_dir_label)
+    tab3_rightbox_layout.addRow(tab3_resume_button)
+    tab3_rightbox_layout.addRow(tab3_resume_folder_viewer)
 
     tab3_layout.addWidget(tab3_leftbox)
     tab3_layout.addWidget(tab3_rightbox)
