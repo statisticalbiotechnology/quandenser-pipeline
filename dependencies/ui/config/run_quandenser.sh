e4803fe74c64f72bbc5e83c0f595a8ed0d69fd4c
Timothy Bergstrom <timothy.bergstrom@gmail.com>
2019-03-04 11:46:00 +0100

    Added email notification to sh

--- dependencies/ui/config/run_quandenser.sh
+++ dependencies/ui/config/run_quandenser.sh
@@ -4,6 +4,7 @@
 PROFILE="local"
 OUTPUT_PATH=""
 OUTPUT_PATH_LABEL=""
+EMAIL_NOTIFICATION=""
 
 # Static
 CONFIG_LOCATION="/home/$USER/.quandenser_pipeline"
@@ -12,7 +13,6 @@ NF_CONFIG_LOCATION="$CONFIG_LOCATION/nf.config"
 NEXTFLOW_PIPELINE="$CONFIG_LOCATION/run_quandenser.nf"
 SINGULARITY_IMAGE="SingulQuand.SIF"  # Note: This means that the pipe reader needs to be in same directory as image!!
 SINGULARITY_ENABLE="-with-singularity"
-
 OUTPUT_PATH="$OUTPUT_PATH$OUTPUT_PATH_LABEL"  # Modify output path after workpath has been set
 STDOUT_FILE="$OUTPUT_PATH/stdout.txt"
 NEXTFLOW_OUTPUTS="-with-trace $OUTPUT_PATH/Nextflow_output/trace.txt -with-report $OUTPUT_PATH/Nextflow_output/report.html -with-timeline $OUTPUT_PATH/Nextflow_output/timeline.html -with-dag $OUTPUT_PATH/Nextflow_output/flowchart.html"
@@ -20,7 +20,8 @@ NEXTFLOW_OUTPUTS="-with-trace $OUTPUT_PATH/Nextflow_output/trace.txt -with-repor
 mkdir -p "$OUTPUT_PATH/Nextflow_output"  # Will also create output folder if it does not exist
 
 $CONFIG_LOCATION/nextflow run $NEXTFLOW_PIPELINE $SINGULARITY_ENABLE $SINGULARITY_IMAGE \
--c $NF_CONFIG_LOCATION $NEXTFLOW_OUTPUTS -w $WORK_DIRECTORY -profile $PROFILE | tee -a $STDOUT_FILE
+-c $NF_CONFIG_LOCATION $NEXTFLOW_OUTPUTS -w $WORK_DIRECTORY -profile $PROFILE $EMAIL_NOTIFICATION \
+| tee -a $STDOUT_FILE
 
 # Notes: by using "-with-weblog", you could send the output to a url (HTTP POST request)
 # This means you could get notified with your webserver when the pipeline is finisheds
