3d0e302831b9f6712a0efd32fc98f98d80d71f01
Timothy Bergstrom <timothy.bergstrom@gmail.com>
2019-03-22 10:40:35 +0100

    Removed old files

--- old/run_all.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo singularity build singulqand.simg Singularity
-# curl -s https://get.nextflow.io | bash
-# ./nextflow main.nf -with-singularity singulqand.simg
-nextflow main.nf -with-singularity singulqand.simg
