6b5842ae699495d93e657256ee90fb2602cb9c9d
Timothy Bergstrom <timothy.bergstrom@gmail.com>
2019-03-22 16:10:22 +0100

    Changed warning print for warning box + removed typo

--- dependencies/ui/utils.py
+++ dependencies/ui/utils.py
@@ -10,7 +10,11 @@ from PySide2.QtWidgets import QMessageBox
 from custom_config_parser import custom_config_parser
 
 def ERROR(message):
-    print(Fore.RED + f"ERROR: {message}" + Fore.RESET)
+    msg = QMessageBox()
+    msg.setIcon(QMessageBox.Warning)
+    msg.setWindowTitle("ERROR")
+    msg.setText(message)
+    msg.exec_()
 
 def WARNING(message):
     print(Fore.YELLOW + f"WARNING: {message}" + Fore.RESET)
@@ -101,7 +105,7 @@ def check_running(config_path):
             if not crash:
                 msg.setIcon(QMessageBox.Information)
                 msg.setWindowTitle("Started")
-                msg.setText("Quandenser started. P")
+                msg.setText("Quandenser started")
                 msg.setDetailedText(f"PID of the process is: {pid}")
             else:
                 msg.setIcon(QMessageBox.Warning)
