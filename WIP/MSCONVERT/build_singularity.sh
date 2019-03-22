35b9cb19673038f5b913e17865fc9f30dd9686c9
Timothy Bergstrom <timothy.bergstrom@gmail.com>
2019-03-22 10:41:25 +0100

    Cleared old files from WIP

--- WIP/MSCONVERT/build_singularity.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo singularity build --sandbox singulqand.sandbox Singularity
--- WIP/MSCONVERT/link_wine.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-for object in `ls $WINEPREFIX`;
-    do ln -sf -T $WINEPREFIX/$object /tmp/wineprefix64_$USER/wineprefix64_$1/$object;
-done
