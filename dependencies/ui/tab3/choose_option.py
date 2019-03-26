6023d68753550251cea6889c640df667427f91ce
Timothy Bergstrom <timothy.bergstrom@gmail.com>
2019-03-26 14:24:50 +0100

    Simple true/false combobox

---
+++ dependencies/ui/tab3/choose_option.py
@@ -0,0 +1,24 @@
+from PySide2.QtWidgets import QComboBox, QVBoxLayout, QFrame, QTableWidget, QLabel, QStackedLayout
+
+from custom_config_parser import custom_config_parser
+from tooltip_label import tooltip_label
+from tab2.parameter_setter import parameter_setter_single
+
+class choose_option(QComboBox):
+
+    def __init__(self, parameter, settings_file):
+        super(choose_option,self).__init__(parent = None)
+        self.parameter = parameter
+        self.addItems(["true", "false"])
+        self.parser = custom_config_parser()
+        self.parser.load(settings_file)
+        self.settings_file = settings_file
+        self.default()
+        self.currentIndexChanged.connect(self.selectionchange)
+
+    def selectionchange(self,i):
+        self.parser.write(f"{self.parameter}", self.currentText(), isString=False)
+
+    def default(self):
+        index = self.findText(self.parser.get(f"{self.parameter}"))
+        self.setCurrentIndex(index)
