2b9bbb20f84b232cb886c10f3bfe7c6fd047b0ad
Timothy Bergstrom <timothy.bergstrom@gmail.com>
2019-03-29 10:21:43 +0100

    Added so a return button is added when a new url is opened

--- dependencies/ui/tab5/about.py
+++ dependencies/ui/tab5/about.py
@@ -1,4 +1,5 @@
 from PySide2.QtWebEngineWidgets import QWebEngineView
+from PySide2.QtWidgets import QPushButton
 from PySide2.QtCore import QUrl
 import os
 
@@ -6,7 +7,23 @@ class about(QWebEngineView):
 
     def __init__(self):
         super(about,self).__init__(parent = None)
-        current_dir = os.getcwd().replace('\\', '/')  # Windows fix
+        self.current_dir = os.getcwd().replace('\\', '/')  # Windows fix
         #html_file = open("ui/tab5/about.html", 'r', encoding='utf-8')
         #self.setHtml(html_file.read())  # THIS WILL NOT LOAD IMAGES
-        self.load(QUrl.fromLocalFile(f"{current_dir}/tab5/about.html"))
+        self.urlChanged.connect(self.check_url)
+        self.return_button = QPushButton()
+        self.return_button.setText('BACK TO ABOUT')
+        self.return_button.clicked.connect(self.return_to_about)
+        self.return_button.setStyleSheet('QPushButton {color: #00ff96;}')
+        self.load(QUrl.fromLocalFile(f"{self.current_dir}/tab5/about.html"))
+
+    def check_url(self, url):
+        if "tab5/about.html" in str(url):
+            pass
+        else:
+            parent_layout = self.parentWidget().layout()
+            parent_layout.addWidget(self.return_button)
+
+    def return_to_about(self):
+        self.load(QUrl.fromLocalFile(f"{self.current_dir}/tab5/about.html"))
+        self.return_button.setParent(None)
