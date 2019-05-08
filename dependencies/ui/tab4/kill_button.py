c88959e724e7a8af7acc2c48807dd5bd2fe7f241
Timothy Bergstrom <timothy.bergstrom@gmail.com>
2019-05-08 08:15:46 +0200

    Added so kill button will add "KILLED" to the run when you press it

--- dependencies/ui/tab4/kill_button.py
+++ dependencies/ui/tab4/kill_button.py
@@ -5,10 +5,12 @@ from colorama import Fore, Back, Style
 
 class kill_button(QPushButton):
 
-    def __init__(self, pid):
+    def __init__(self, pid, jobs_path, job):
         super(kill_button,self).__init__(parent = None)
         self.setText('KILL')
         self.pid = pid  # will use pid as ppid
+        self.jobs_path = jobs_path
+        self.job = job
         self.setStyleSheet("background-color:red")  # Change color depending on if you can run or not
         self.clicked.connect(self.kill)
 
@@ -27,6 +29,7 @@ class kill_button(QPushButton):
                 killed = True
         if killed:
             print(Fore.RED + f"Killed process with pid {self.pid} and its children" + Fore.RESET)
+            self.update_job_file(self.job, 'KILLED')
         else:
             print(Fore.RED + f"FAILED to kill process with pid {self.pid} and its children. Are you on the same login node?" + Fore.RESET)
         parent = self.parentWidget()
@@ -36,3 +39,16 @@ class kill_button(QPushButton):
                 child.update()
                 break
         return 0
+
+    def update_job_file(self, job_done, label):
+        with open(self.jobs_path, 'r') as jobfile:
+            all_jobs = jobfile.readlines()
+
+        for index, job in enumerate(all_jobs):
+            if job == '\t'.join(job_done):
+                job_done = '\t'.join(job_done).replace('\n', '')
+                all_jobs[index] = job_done + '\t' + label + '\n'
+
+        with open(self.jobs_path, 'w') as jobfile:
+            for job in all_jobs:
+                jobfile.write(job)
