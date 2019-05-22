4be1c982fdcb81fe13413d5ab2bfbbe9c3061da7
Timothy Bergstrom <timothy.bergstrom@gmail.com>
2019-05-22 14:19:06 +0200

    Added so induvidual plots are added

--- doc/thesis/results/stats_triqler.py
+++ doc/thesis/results/stats_triqler.py
@@ -34,14 +34,19 @@ def unique_and_diff(directory):
             line = line.replace('\n','').split('\t')
             qval = float(line[0])
             if qval < threshold:
-                proteins.append(line[header.index('protein')])
+                protein = line[header.index('protein')]
+                protein = protein.split('|')[-1]
+                proteins.append(protein)
                 diff += 1
     print("DIFF:", diff)
     print("UNIQUE:", len(list(set(proteins))))
-    print(set(proteins))
     return list(set(proteins))
 
 def protein_search(proteins):
+    with open('proteins.txt', 'w') as f:
+        for protein in proteins:
+            f.write(protein.split('|')[-1] + '\n')
+
     url = "https://www.ncbi.nlm.nih.gov/gene/?term=REPLACE"
     for protein in proteins:
         pass
@@ -57,7 +62,7 @@ def generate_heatmap(file, proteins):
                 header = line.replace('\n','').split('\t')
                 continue
             line = line.replace('\n','').split('\t')
-            protein = line[header.index('protein')]
+            protein = line[header.index('protein')].split('|')[-1]
             if protein in proteins:
                 fold_change = float(line[header.index('log2_fold_change')])
                 comb = os.path.basename(file)
@@ -65,8 +70,49 @@ def generate_heatmap(file, proteins):
                 if protein not in data.keys():
                     data[protein] = {}
                 data[protein][comb] = fold_change
+    generate_single_heatmap(proteins, data)
+    generate_combined_heatmap(proteins, data)
 
+def generate_single_heatmap(proteins, data):
+    if not os.path.isdir('proteins'):
+        os.mkdir('proteins')
     nrows = int(len(proteins)/2)
+    groups = list(data.values())[0].keys()
+    for num_prot, protein in enumerate(data.keys()):
+        matrix, comb_matrix, xlabel, ylabel = generate_heatmap_layout(list(groups))
+        for comb in data[protein].keys():
+            for row,x in enumerate(xlabel):
+                for column,y in enumerate(ylabel):
+                    comb_check = x + 'vs' + y
+                    if comb_check == comb:
+                        matrix[row][column] = data[protein][comb]
+        # Fill in reverse
+        for i in range(len(xlabel)):
+            for j in range(i, len(ylabel)):
+                if matrix[i][j] == 0:
+                    matrix[j][i] = None
+                else:
+                    matrix[j][i] = -matrix[i][j]
+        df = pd.DataFrame(data=matrix,
+                          columns=xlabel)
+        sns.heatmap(df,
+                    vmin = -3.0,
+                    vmax = 3.0,
+                    cmap = "RdBu_r",
+                    annot = True,
+                    xticklabels=df.columns,
+                    yticklabels=df.columns,
+                    cbar_kws={'label': 'Log2 Fold change'},
+                    annot_kws={"size": 20})
+        plt.title(protein, fontsize=26)
+        print(f"{num_prot} out of {len(proteins)} done", end='\r')
+        plt.savefig(f"proteins/{protein}.png", bbox_inches='tight',pad_inches=0)
+        plt.close()
+
+def generate_combined_heatmap(proteins, data):
+    nrows = int(len(proteins)/2)
+    if len(proteins)%2 != 0:
+        nrows+=1
     fig, ax = plt.subplots(nrows=nrows, ncols=2, figsize=(20,30))
     fig.subplots_adjust(hspace=0.3)
     groups = list(data.values())[0].keys()
