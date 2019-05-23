de30ddd0200d8d1589dbce3eefcb2ef97408d503
Timothy Bergstrom <timothy.bergstrom@gmail.com>
2019-05-23 15:48:12 +0200

    Fixed typos

--- doc/thesis/results/plots.py
+++ doc/thesis/results/plots.py
@@ -13,10 +13,10 @@ times_cyano = [1*60 + 12,
                1*60 + 19,  # CALC OR FIND
                1*60 + 16,
                2*60 + 41]
-times_ralstonia_core = [90
-                        78,
-                        1,
-                        712.5]
+times_cyano_core = [90,
+                    78,
+                    1,
+                    712.5]
 # Ralstonia
 times_ralstonia = [3*60 + 23,
                    3*60 + 21,  # CALC OR FIND
@@ -40,7 +40,7 @@ data = {'Time': {'Data': [times_cyano, times_ralstonia],
                               'titles': ['cyano', 'ralstonia']}}
 
 # Labels
-programs = ['Quandenser pipeline\n(parallel)', 'Quandenser pipeline\n(non-parallel)', 'OpenMS', 'MaxQuant']
+programs = ['QP \n(parallel)', 'QP \n(non-parallel)', 'OpenMS', 'MaxQuant']
 colors = [(45,55,65,0), (45,55,65,0), (255,160,0,0), (0,40,160,0)]
 colors = [(r/255, g/255, b/255, a) for (r,g,b,a) in colors]
 
@@ -52,11 +52,12 @@ for key in data.keys():
         d = pd.DataFrame({' ': programs, key: d})
         plt.subplot(1,2,index+1)
         #plt.title(titles[index]  + ' ' + key)
-        barplot_fig = sns.barplot(x=' ', y=key, data=d, order=programs, palette=colors)
+        barplot_fig = sns.barplot(x=' ', y=key, data=d, order=programs, palette=colors, )
         plt.ylabel(data[key]['ylabel'])
         for item in barplot_fig.get_xticklabels():
-            item.set_rotation(20)
+            item.set_rotation(0)
         for p in barplot_fig.patches:
             barplot_fig.annotate(str(p.get_height()), (p.get_x() * 1.005, p.get_height() * 1.005))
-    plt.savefig(data[key]['file'])
+    #plt.tight_layout()
+    plt.savefig(data[key]['file'], bbox_inches = "tight")
     plt.close()
