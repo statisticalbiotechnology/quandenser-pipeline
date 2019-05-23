2001b45c577e88d60bfeb86d88151f6995d44cf4
Timothy Bergstrom <timothy.bergstrom@gmail.com>
2019-05-23 14:22:29 +0200

    Working on core hour plot

--- doc/thesis/results/plots.py
+++ doc/thesis/results/plots.py
@@ -4,18 +4,28 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from scipy import stats
 import datetime
-sns.set(color_codes=True)
+sns.set_color_codes("dark")
+sns.set_style("white")
+sns.set_context("talk")
 
 # Cyano
 times_cyano = [1*60 + 12,
                1*60 + 19,  # CALC OR FIND
                1*60 + 16,
                2*60 + 41]
+times_ralstonia_core = [90
+                        78,
+                        1,
+                        712.5]
 # Ralstonia
 times_ralstonia = [3*60 + 23,
                    3*60 + 21,  # CALC OR FIND
                    3*60 + 52,
                    8*60 + 11]
+times_ralstonia_core = [246,
+                        204,
+                        1,
+                        1]
 
 # Titles
 titles =  ['Cyanobacteria', 'Ralstonia']
@@ -23,7 +33,11 @@ titles =  ['Cyanobacteria', 'Ralstonia']
 data = {'Time': {'Data': [times_cyano, times_ralstonia],
                  'ylabel': 'Minutes',
                  'file': 'times.png',
-                 'titles': ['cyano', 'ralstonia']}}
+                 'titles': ['cyano', 'ralstonia']},
+        'Time core hours':  {'Data': [times_cyano_core, times_ralstonia_core],
+                              'ylabel': 'Minutes',
+                              'file': 'times_core.png',
+                              'titles': ['cyano', 'ralstonia']}}
 
 # Labels
 programs = ['Quandenser pipeline\n(parallel)', 'Quandenser pipeline\n(non-parallel)', 'OpenMS', 'MaxQuant']
@@ -37,7 +51,7 @@ for key in data.keys():
     for index, d in enumerate(data[key]['Data']):
         d = pd.DataFrame({' ': programs, key: d})
         plt.subplot(1,2,index+1)
-        plt.title(titles[index]  + ' ' + key)
+        #plt.title(titles[index]  + ' ' + key)
         barplot_fig = sns.barplot(x=' ', y=key, data=d, order=programs, palette=colors)
         plt.ylabel(data[key]['ylabel'])
         for item in barplot_fig.get_xticklabels():
