dbf6bcb2cb4cba0acb8c79ed38b9f3f39a230987
Timothy Bergstrom <timothy.bergstrom@gmail.com>
2019-09-11 16:00:39 +0200

    Updated command wrapper. It should work now

--- dependencies/command_wrapper.py
+++ dependencies/command_wrapper.py
@@ -10,6 +10,8 @@ args = parser.parse_args()
 def main():
     print(f"running command: {args.command}")
     process = subprocess.Popen([args.command],
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE,
                                 shell=True)
 
     checker = {'msconvert': False,
@@ -27,14 +29,33 @@ def main():
     while True:
         poll = process.poll()
         if poll == None:
+
             if checker['msconvert']:
                 with open('stdout.txt', 'r') as file:
                     lines = file.readlines()
+                    # The crash happens in convertin spectra. Wait until it starts before checking
+                    if not 'converting spectra:' in lines[-1]:
+                        continue
                     if lines[-1] == last_line:
                         counter += 1
+                    elif lines[-1] != last_line:
+                        counter = 0  # Reset counter every time here
+                    last_line = lines[-1]  # Set last line as the line you read
+                if counter >= 3:  # Check 3 times if the line is the same
+                    raise Exception('Process wrapper: MSconvert is stuck.')
+
+            elif checker['dinosaur']:
+                with open('stdout.txt', 'r') as file:
+                    lines = file.readlines()
+                for line in lines:
+                    if 'Exception in thread "main" java.lang.ClassCastException' in line:
+                        raise Exception('Process wrapper: Dinosaur in Quandenser has crashed.')
         else:
-            break
-        time.sleep(5)
+            break  # If process had stopped, exit here
+        time.sleep(7)  # 7 seconds between checks
+    if process.returncode != 0:
+        stdout, stderr = process.communicate()
+        raise Exception(f"Process wrapper: Unknown error. Stderr:\n {stderr.decode('utf-8')}")
 
 if __name__ == '__main__':
     main()
