07ec8d821e5aa7ce4325563df96e4168050a120d
Timothy Bergstrom <timothy.bergstrom@gmail.com>
2019-09-17 12:15:02 +0200

    Updated non-parallel local run time

--- doc/paper/data/generate_plots.py
+++ doc/paper/data/generate_plots.py
@@ -9,7 +9,7 @@ sns.set_style("white")
 sns.set_context("talk")
 data = {
 'QP\n(parallel)': [10*60 + 46, 60 + 13, 56],
-'QP\n(non-parallel)': [0, 60 + 35, 60 + 12],
+'QP\n(non-parallel)': [10*60 + 57, 60 + 35, 60 + 12],
 'QP HPC\n(parallel)': [0, 60 + 17, 60 + 21],
 'QP HPC\n(non-parallel)': [0, 2*60 + 29, 60 + 54],
 'MaxQuant\nmbr': [28*60 + 44, 4*60 + 17, 60 + 45]
