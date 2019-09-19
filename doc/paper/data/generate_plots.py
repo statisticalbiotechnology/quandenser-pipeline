2d32ea2b349023015418e15674e9aab7b6117636
Timothy Bergstrom <timothy.bergstrom@gmail.com>
2019-09-19 11:27:34 +0200

    Updated so the data sets are group, not the programs

--- doc/paper/data/generate_plots.py
+++ doc/paper/data/generate_plots.py
@@ -8,27 +8,30 @@ sns.set_color_codes("dark")
 sns.set_style("white")
 sns.set_context("talk")
 data = {
-'QP\n(parallel)': [10*60 + 46, 60 + 13, 56],
-'QP\n(non-parallel)': [10*60 + 57, 60 + 35, 60 + 12],
-'QP HPC\n(parallel)': [0, 60 + 17, 60 + 21],
-'QP HPC\n(non-parallel)': [0, 2*60 + 29, 60 + 54],
-'MaxQuant\nmbr': [28*60 + 44, 4*60 + 17, 60 + 45]
+'QP (parallel)': [10*60 + 46, 60 + 13, 56],
+'QP (non-parallel)': [10*60 + 57, 60 + 35, 60 + 12],
+'QP HPC (parallel)': [0, 60 + 17, 60 + 21],
+'QP HPC (non-parallel)': [14*60 + 4, 2*60 + 29, 60 + 54],
+'MaxQuant mbr': [28*60 + 44, 4*60 + 17, 60 + 45]
 }
 data_cores = {
-'QP\n(parallel)': [1,2,3],
-'QP\n(non-parallel)': [1,2,3],
-'QP HPC\n(parallel)': [1,2,3],
-'QP HPC\n(non-parallel)': [1,2,3],
-'MaxQuant\nmbr': [1,2,3]
+'QP (parallel)': [29.8, 3.9, 2.4],
+'QP (non-parallel)': [16.9, 3.0, 1.8],
+'QP HPC (parallel)': [1, 23.9, 12.6],
+'QP HPC (non-parallel)': [210.9, 29.0, 18.2],
+'MaxQuant mbr': [103.7, 15.5, 6.2]
 }
 
 # Restructure
 data_sets =  ['Geyer (180 files)', 'Bracht (27 files)', 'Latosinska (8 files)']
 data_list = []
-for key in data.keys():
-    values = data[key]
-    for index,value in enumerate(values):
-        data_list.append([key, data_sets[index], value])
+data_cores_list = []
+for key_time, key_cores in zip(data.keys(), data_cores):
+    time_values = data[key_time]
+    cpu_values = data[key_cores]
+    for index, (time, cpu) in enumerate(zip(time_values, cpu_values)):
+        data_list.append([key_time, data_sets[index], time])
+        data_cores_list.append([key_cores, data_sets[index], cpu])
 
 y = 'Minutes'
 file = 'times.png'
@@ -36,12 +39,21 @@ columns = ['run', 'data_set', 'time']
 pd_data = pd.DataFrame(data_list, columns = columns)
 print(pd_data)
 plt.figure(figsize=(15,7))
-ax = sns.barplot(x='run',
+ax = sns.barplot(x='data_set',
                  y='time',
-                 hue='data_set',
+                 hue='run',
                  data=pd_data)
+
+# Remove uneccessary titles
 handles, labels = ax.get_legend_handles_labels()
 ax.legend(handles=handles[0:], labels=labels[0:])
+ax.set_xlabel('')
+
+# Set values on each bar
+for p in ax.patches:
+    ax.text(p.get_x() + p.get_width()/2., p.get_height(), '%d' % int(p.get_height()),
+            fontsize=12, color='black', ha='center', va='bottom')
+
 plt.ylabel(y)
 plt.tight_layout()
 plt.savefig(file)
