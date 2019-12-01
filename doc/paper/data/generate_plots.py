396c4a93bf0f640c4c796883a23811573c1bb4a4
TimothyBergstrom <timothy.bergstrom@gmail.com>
2019-12-01 21:54:27 +0100

    Fixed so generate_plots uses replicates for error bars

--- doc/paper/data/generate_plots.py
+++ doc/paper/data/generate_plots.py
@@ -9,14 +9,13 @@ sns.set_color_codes("dark")
 sns.set_style("white")
 sns.set_context("talk")
 
-def s2m(time_str):
+def string2time(time_str):
     """Get minutes from time."""
     h, m, s = time_str.split(':')
     return int(h) * 60 + int(m) + int(s)/60
 
 data_list = []
 data_cores_list = []
-
 data_sets =  ['Geyer (118 files)',
               'Bracht (27 files)',
               'Latosinska (8 files)']
@@ -26,13 +25,23 @@ run_types =  ['QP (parallel)',
               'QP HPC (non-parallel)',
               'MaxQuant mbr']
 
-for data_set in data_sets:
-    for run_type in run_types:
-        time_values = data_times[data_set][run_type]
-        core_values = data_cores[data_set][run_type]
-        for time_value, core_value in zip(time_values, core_values):
-            data_list.append([run_type, data_set, time_value])
-            data_cores_list.append([run_type, data_set, core_value])
+with open('data.csv', 'r') as f:
+    for idx, line in enumerate(f):
+        if idx == 0 or idx == 17 or idx == 16:
+            continue
+        line = line.split(';')
+        data_set = line[0]
+        run = line[1]
+        replicates = int(line[2])
+        values = line[3:3+replicates]
+        for value in values:
+            if idx < 17:
+                value = string2time(value)
+                data_list.append([run, data_set, int(value)])
+            else:
+                value = value.replace(',', '.')
+                value = float(value)
+                data_cores_list.append([run, data_set, value])
 print(data_list)
 
 for d, y, f in zip([data_list, data_cores_list], ['Minutes', 'Core hours'], ['times.png', 'cores.png']):
@@ -64,7 +73,7 @@ for d, y, f in zip([data_list, data_cores_list], ['Minutes', 'Core hours'], ['ti
             display = '%.1f'
         for p in ax.patches:
             ax.text(p.get_x() + p.get_width()/2., p.get_height(), display % round(p.get_height(),2),
-                    fontsize=12, color='black', ha='center', va='bottom')
+                    fontsize=12, color='black', ha='right', va='bottom')
 
     plt.tight_layout()
     plt.savefig(file)
